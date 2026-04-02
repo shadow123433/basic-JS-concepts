4254e27ff792d0cf69694bc322859bb01ed621cb
Thiago martins <thiagoribeirodossantos965@gmail.com>
2026-04-02 19:05:48 -0300

    aprendendo for in e for of basico

---
+++ for in e for of/for in.js
@@ -0,0 +1,8 @@
+const users = {
+    name: "thiago",
+    age: 30,
+    city: "sao paulo"
+}
+for (let key in users) {
+    console.log(key+": "+users[key]);
+}
---
+++ for in e for of/for of.js
@@ -0,0 +1,7 @@
+const users = ["thiago", "leticia", "pietra"];
+for(let name of users) {
+  console.log(name);
+}
+
+//exemplo pratico de for of
+
